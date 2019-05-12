f042aee6daea733a1423b26047c3a2d52106ad26
etien-andres <andres.etien@gmail.com>
2019-05-12 10:53:38 -0500

    cambiado nombre de variable
    para que sea mas legible

--- js/logic.js
+++ js/logic.js
@@ -1,22 +1,12 @@
 
-
-
-
-
 firebase.database().goOnline();
 firebase.database().ref('estadoActual/temp').once('value',function (snapshot) {
     console.log(snapshot.val());
     document.getElementById("tempe").innerText=snapshot.val();
-
 });
 
-
-
-
-
-
-function changetemp(temp) {
-    var temperaturas = {
+function changeVarControl(temp) {
+    var variablesControl = {
         AireF:0,
         AireC:0,
         Bomba:0,
@@ -25,11 +15,11 @@ function changetemp(temp) {
         tempDesiredMax: temp+10,
     };
 
-
     var updates = {};
-    updates['/Control/'] = temperaturas;
+    updates['/Control/'] = variablesControl;
     return firebase.database().ref().update(updates);
 }
+
 setTimeout(function () {
    // console.log("hola");
     location.reload();
