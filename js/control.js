e42309b2513f2b202ab623f483921c21ac19beb0
etien-andres <andres.etien@gmail.com>
2019-05-13 00:18:06 -0500

    control completo
    control y vision de datos completo

---
+++ js/control.js
@@ -0,0 +1,93 @@
+firebase.database().goOnline();
+let variablesdecontrol=new Object();
+
+window.onload=function(){
+    firebase.database().ref('Control/Pelt').once('value',function (snapshot) {
+        variablesdecontrol.Pelt=snapshot.val();
+    });
+        firebase.database().ref('Control/Bomba').once('value',function (snapshot) {
+    if (snapshot.val()==0)     $('#bomba').bootstrapToggle('off');
+    else if (snapshot.val()==1) $('#bomba').bootstrapToggle('on');
+       variablesdecontrol.Bomba=snapshot.val();
+    });
+
+    firebase.database().ref('Control/AireC').once('value',function (snapshot) {
+        if (snapshot.val()==0)     $('#acalient').bootstrapToggle('off');
+        else if (snapshot.val()==1) $('#acalient').bootstrapToggle('on');
+        variablesdecontrol.AireC=snapshot.val();
+
+    });
+    firebase.database().ref('Control/AireF').once('value',function (snapshot) {
+        if (snapshot.val()==0)     $('#aFrio').bootstrapToggle('off');
+        else if (snapshot.val()==1) $('#aFrio').bootstrapToggle('on');
+        variablesdecontrol.AireF=snapshot.val();
+
+    });
+    firebase.database().ref('Control/tempDesired').once('value',function (snapshot) {
+        document.getElementById('tempdesi').value = snapshot.val();
+        variablesdecontrol.tempDesired=snapshot.val();
+
+    });
+
+
+    $('#bomba').change(function() {
+        if (variablesdecontrol.Bomba==0)    {
+            variablesdecontrol.Bomba=1;
+            changeVarControl(variablesdecontrol);
+        }
+        else if (variablesdecontrol.Bomba==1){
+            variablesdecontrol.Bomba=0;
+            changeVarControl(variablesdecontrol);
+
+
+        }
+
+    });
+
+    $('#acalient').change(function() {
+        if (variablesdecontrol.AireC==0)    {
+            variablesdecontrol.AireC=1;
+            changeVarControl(variablesdecontrol);
+        }
+        else if (variablesdecontrol.AireC==1){
+            variablesdecontrol.AireC=0;
+            changeVarControl(variablesdecontrol);
+
+
+        }
+
+    });
+    $('#aFrio').change(function() {
+        if (variablesdecontrol.AireF==0)    {
+            variablesdecontrol.AireF=1;
+            changeVarControl(variablesdecontrol);
+        }
+        else if (variablesdecontrol.AireF==1){
+            variablesdecontrol.AireF=0;
+            changeVarControl(variablesdecontrol);
+
+
+        }
+
+    });
+
+    document.getElementById('tempdesi').oninput =function () {
+        variablesdecontrol.tempDesired=document.getElementById('tempdesi').value;
+        changeVarControl(variablesdecontrol);
+    };
+
+
+
+
+
+};
+
+function changeVarControl(variablesControl) {
+
+
+    var variablesControl = variablesControl;
+
+    var updates = {};
+    updates['/Control/'] = variablesControl;
+    return firebase.database().ref().update(updates);
+}
--- js/logic.js
+++ js/logic.js
@@ -1,29 +1,95 @@
 
 firebase.database().goOnline();
-firebase.database().ref('estadoActual/temp').once('value',function (snapshot) {
-    console.log(snapshot.val());
-    document.getElementById("tempe").innerText=snapshot.val();
-});
 
-function changeVarControl(temp) {
-    var variablesControl = {
-        AireF:0,
-        AireC:0,
-        Bomba:0,
-        Pelt:0,
-        tempDesired: temp,
-        tempDesiredMax: temp+10,
-    };
+let temp=20,nivel,humedadC,humedad,agua=23;
 
-    var updates = {};
-    updates['/Control/'] = variablesControl;
-    return firebase.database().ref().update(updates);
+
+
+getTempe();
+function getTempe() {
+    // and write tempe
+    firebase.database().ref('estadoActual/temp').once('value',function (snapshot) {
+        document.getElementById("tempe").innerText=snapshot.val()+' °C';
+
+        if (snapshot.val()<(temp+3)&&snapshot.val()>(temp-3)){
+            var element = document.getElementById("tempe");
+            element.classList.remove("table-danger");
+
+            element.classList.add("table-success");
+
+        }
+        if (snapshot.val()<(temp-3)||snapshot.val()>(temp+3)){
+            var element = document.getElementById("tempe");
+            element.classList.remove("table-success")
+
+            element.classList.add("table-danger");
+        }
+
+    });
+    firebase.database().ref('estadoActual/Nivel Agua').once('value',function (snapshot) {
+        document.getElementById('nivel').innerText=snapshot.val()+' %';
+        if (snapshot.val()>80){
+            var element = document.getElementById("nivel");
+            element.classList.remove("table-danger");
+
+            element.classList.add("table-success");
+
+        }
+        if (snapshot.val()<80){
+            var element = document.getElementById("nivel");
+            element.classList.remove("table-success")
+
+            element.classList.add("table-danger");
+
+        }
+    });
+    firebase.database().ref('estadoActual/HumedadCam').once('value',function (snapshot) {
+        document.getElementById('humedadC').innerText=snapshot.val()+' %';
+        if (snapshot.val()>75){
+            var element = document.getElementById("humedadC");
+            element.classList.remove("table-danger");
+
+            element.classList.add("table-success");
+        }
+        if (snapshot.val()<75){
+            var element = document.getElementById("humedadC");
+            element.classList.remove("table-success")
+
+            element.classList.add("table-danger");
+
+        }
+    });
+    firebase.database().ref('estadoActual/Humedad').once('value',function (snapshot) {
+        document.getElementById('humedad').innerText=snapshot.val()+' %';
+        if (snapshot.val()>10){
+            var element = document.getElementById("humedad");
+            element.classList.remove("table-danger");
+
+            element.classList.add("table-success");
+        }
+        if (snapshot.val()<10){
+            var element = document.getElementById("humedad");
+            element.classList.remove("table-success")
+            element.classList.add("table-danger");
+
+        }
+    });
+    firebase.database().ref('estadoActual/temperatura agua').once('value',function (snapshot) {
+        document.getElementById('agua').innerText=snapshot.val()+' °C';
+        if (snapshot.val()>agua-3&&snapshot.val()>agua-3){
+            var element = document.getElementById("agua");
+            element.classList.remove("table-danger");
+            element.classList.add("table-success");
+        }
+        if (snapshot.val()<=agua-3||snapshot.val()>=agua+3){
+            var element = document.getElementById("agua");
+            element.classList.remove("table-success")
+            element.classList.add("table-danger");
+
+        }
+    });
+    setTimeout(getTempe,500);
 }
-
-setTimeout(function () {
-   // console.log("hola");
-    location.reload();
-},8000);
 
 
 
